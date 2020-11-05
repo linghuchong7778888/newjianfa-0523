1e61f6d9ad961993117498c2c1d2084245742699
xjc <784864663@qq.com>
2020-11-05 10:36:16 +0800

    xjc update2

--- src/main/java/com/xijingcong/git/Dugujiujian.java
+++ src/main/java/com/xijingcong/git/Dugujiujian.java
@@ -3,5 +3,6 @@ package com.xijingcong.git;
 public class Dugujiujian {
     public static void main(String[] args) {
         System.out.println("XXXXX");
+        System.out.println("JJJJJ");
     }
 }
