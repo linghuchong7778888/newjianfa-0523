62a40c80819f7c49282b2c8cf2109c8671558b16
linghuchong7778888 <442344413@qq.com>
2020-11-05 10:37:00 +0800

    linghu update2

--- src/main/java/com/xijingcong/git/Dugujiujian.java
+++ src/main/java/com/xijingcong/git/Dugujiujian.java
@@ -2,6 +2,7 @@ package com.xijingcong.git;
 
 public class Dugujiujian {
     public static void main(String[] args) {
-        
+        System.out.println("XXXXX");
+        System.out.println("CCCCC");
     }
 }
